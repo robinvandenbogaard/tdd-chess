bb59d9b8f99b36d4ac4b0c67a2608e8df70e538b
robinvandenbogaard <robin.van.den.bogaard@topicus.nl>
2022-04-06 23:47:32 +0200

    added test for rook notallowed movements

--- src/test/java/nl/roka/chess/move/RookMovementStrategyTest.java
+++ src/test/java/nl/roka/chess/move/RookMovementStrategyTest.java
@@ -1,12 +1,14 @@
 package nl.roka.chess.move;
 
-import io.vavr.collection.List;
+import io.vavr.collection.HashSet;
+import io.vavr.collection.Set;
 import nl.roka.chess.piece.Piece;
 import nl.roka.chess.piece.PieceFactory;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static nl.roka.chess.move.MoveType.Attack;
+import static nl.roka.chess.move.MoveType.NotAllowed;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -15,12 +17,17 @@ class RookMovementStrategyTest {
 	private final static Piece rook = new PieceFactory().whiteRook();
 	private final static Position startingPosition = Position.position("d4");
 
-	public static List<Position> rookCanMoveHorizontally() {
-		return List.of("a4", "b4", "c4", "e4", "f4", "g4", "f4").map(Position::position);
+	public static Set<Position> rookCanMoveHorizontally() {
+		return HashSet.of("a4", "b4", "c4", "e4", "f4", "g4", "h4").map(Position::position);
 	}
 
-	public static List<Position> rookCanMoveVertically() {
-		return List.of("d1", "d2", "d3", "d5", "d6", "d7", "d8").map(Position::position);
+	public static Set<Position> rookCanMoveVertically() {
+		return HashSet.of("d1", "d2", "d3", "d5", "d6", "d7", "d8").map(Position::position);
+	}
+
+	public static Set<Position> rookCannotMoveOtherThenOverStraightAxis() {
+		return PositionCollections.allPositions.remove(startingPosition).removeAll(rookCanMoveVertically())
+											   .removeAll(rookCanMoveHorizontally());
 	}
 
 	@ParameterizedTest
@@ -35,5 +42,13 @@ class RookMovementStrategyTest {
 	void rookCanMoveVertically(Position target) {
 		var moveType = rook.getMoveType(startingPosition, target);
 		assertThat(moveType, is(Attack));
+	}
+
+	@ParameterizedTest
+	@MethodSource
+	void rookCannotMoveOtherThenOverStraightAxis(Position target) {
+		var moveType = rook.getMoveType(startingPosition, target);
+		assertThat(moveType, is(NotAllowed));
+
 	}
 }
